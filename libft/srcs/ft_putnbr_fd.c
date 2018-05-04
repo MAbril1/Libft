5875baa12713433b37c366683d5992ad8928f625
Milo Abril <mabril@e1z2r3p40.42.us.org>
2018-05-04 16:03:43 -0700

    file descriptor functions

--- libft/srcs/ft_putnbr_fd.c
+++ libft/srcs/ft_putnbr_fd.c
@@ -10,4 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void		ft_putnbr_fd(int n, int fd)
+{
+	if (nb > 2147483647 || nb <= -2147483648)
+		return ;
+	if (nb < 0)
+		ft_putchar_fd('-', fd);
+	if (nb < 0)
+		nb *= -1;
+	if (nb < 10)
+	{
+		ft_putchar_fd(nb + '0', fd);
+		return ;
+	}
+	ft_putnbr_fd(nb / 10, fd);
+	ft_putchar_fd((nb % 10) + '0', fd);
+	return ;
+}
