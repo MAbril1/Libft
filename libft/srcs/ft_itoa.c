21f2e760821b9322f58588a67a7889ff09134a00
Milo Abril <mabril@e1z2r3p40.42.us.org>
2018-05-04 15:38:56 -0700

    itoa

--- libft/srcs/ft_itoa.c
+++ libft/srcs/ft_itoa.c
@@ -10,4 +10,39 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+static int	get_strlen(int n)
+{
+	int	i;
+
+	i = 0;
+	while (n /= 10)
+		i++;
+	return (i);
+}
+
+static char	*conversion(int n, char *r)
+{
+	char *dst;
+
+	dst = r;
+	if (n < 0)
+	{
+		n *= -1;
+		*dst++ = '-';
+	}
+	else if (n / 10 != 0)
+		dst = conversion(n / 10, dst)
+	*dst++ = (n % 10) + '0';
+	*dst = '\0';
+	return (dst);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*r;
+
+	r = (char *)malloc((get_strlen(n) + 1) * sizeof(*r));
+	return (conversion(n, *r));
+}
